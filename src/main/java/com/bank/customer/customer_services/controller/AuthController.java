d602d056a0b642faf7ad633c3499c3436bcb3337
pepenacho <herreragrej@proton.me>
2025-01-07 22:03:29 -0300

    created dto and AuthController

---
+++ src/main/java/com/bank/customer/customer_services/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.bank.customer.customer_services.controller;
+
+
+import com.bank.customer.customer_services.dto.LoginRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+  private final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
+  @PostMapping("/login")
+  public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+    return null;
+  }
+
+  @PostMapping("/register")
+  public ResponseEntity<?> register(@RequestBody LoginRequest request) {
+    return null;
+  }
+
+  @PostMapping("/roles")
+  public ResponseEntity<?> roles(@RequestBody LoginRequest request) {
+    return null;
+  }
+
+  @PostMapping("/validate")
+  public ResponseEntity<?> validate(@RequestBody LoginRequest request) {
+    return null;
+  }
+}
---
+++ src/main/java/com/bank/customer/customer_services/dto/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.bank.customer.customer_services.dto;
+
+public class LoginRequest {
+}
