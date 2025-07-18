6397644a2f86dc6bd535370d6c46c16633423231
pepenacho <herreragrej@proton.me>
2025-07-18 13:54:43 -0400

    add changes

---
+++ src/main/java/com/bank/auth/auth_services/entity/Role.java
@@ -0,0 +1,16 @@
+package com.bank.auth.auth_services.entity;
+
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@RequiredArgsConstructor
+public class Role {
+  private final Long id;
+  private final String name;
+  private final String description;
+
+  public Long getId() { return id; }
+  public String getName() { return name; }
+  public String getDescription() { return description; }
+}
