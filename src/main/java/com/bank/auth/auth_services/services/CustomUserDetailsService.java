fc4588522e85c7a25925ec38ac0743cc0a062a72
pepenacho <herreragrej@proton.me>
2025-08-11 12:51:18 -0400

    add changes

--- src/main/java/com/bank/auth/auth_services/services/CustomUserDetailsService.java
+++ src/main/java/com/bank/auth/auth_services/services/CustomUserDetailsService.java
@@ -1,14 +1,36 @@
 package com.bank.auth.auth_services.services;
 
+import com.bank.auth.auth_services.model.entity.AuthUser;
+import com.bank.auth.auth_services.model.entity.Role;
+import com.bank.auth.auth_services.repository.AuthUserRepositoryImpl;
+import com.bank.auth.auth_services.repository.RoleRepositoryImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+  private final AuthUserRepositoryImpl authUserRepository;
+  private final RoleRepositoryImpl roleRepositoryImpl;
+
   @Override
-  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-    return null;
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    AuthUser authUser = authUserRepository.findByUserName(username)
+            .get()
+            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+    List<Role> roles = roleRepositoryImpl.findRoleByUserId(authUser.getId()).get();
+    List<SimpleGrantedAuthority> authorities = roles.stream()
+            .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName().toUpperCase()))
+            .toList();
+
+    // Collection<? extends GrantedAuthority> authorities
   }
 }
