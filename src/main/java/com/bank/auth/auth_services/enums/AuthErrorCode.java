fa4915e0070ec89d2f4c2fc297f0ba6957541624
pepenacho <herreragrej@proton.me>
2025-08-25 13:15:06 -0400

    add changes

--- src/main/java/com/bank/auth/auth_services/enums/AuthErrorCode.java
+++ src/main/java/com/bank/auth/auth_services/enums/AuthErrorCode.java
@@ -1,10 +1,25 @@
 package com.bank.auth.auth_services.enums;
 
-public enum AuthErrorCode {
-  USERNAME_ALREADY_EXISTS("Username already exists"),
-  PASSWORD_TOO_WEAK("password too wear"),
-  ACCOUNT_LOCKED("account locked");
+public enum AuthErrorCode implements BaseErrorCode {
+  USERNAME_ALREADY_EXISTS(409, "Username already exists"),
+  PASSWORD_TOO_WEAK(400, "password too wear"),
+  ACCOUNT_LOCKED(423, "account locked");
 
-  AuthErrorCode(String message) {
+  private final int status;
+  private final String message;
+
+  AuthErrorCode(int status, String message) {
+    this.status = status;
+    this.message = message;
+  }
+
+  @Override
+  public int getStatus() {
+    return status;
+  }
+
+  @Override
+  public String getMessage() {
+    return message;
   }
 }
