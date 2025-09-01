fee204412752a782ee1f85456c6b53607ab09a3a
pepenacho <herreragrej@proton.me>
2025-09-01 18:01:55 -0400

    add changes

--- src/main/java/com/bank/auth/auth_services/repository/interfaces/AuthUserRepository.java
+++ src/main/java/com/bank/auth/auth_services/repository/interfaces/AuthUserRepository.java
@@ -1,6 +1,6 @@
 package com.bank.auth.auth_services.repository.interfaces;
 
-import com.bank.auth.auth_services.model.CustomUserDetails;
+import com.bank.auth.auth_services.dto.RegisterRequest;
 import com.bank.auth.auth_services.model.entity.AuthUser;
 import io.vavr.control.Try;
 
@@ -8,4 +8,5 @@ import java.util.Optional;
 
 public interface AuthUserRepository {
   Try<Optional<AuthUser>> findByUserName(String username);
-}
+  Try<AuthUser> save(RegisterRequest request, String hashedPassword);
+ }
