669564393ec4fd8815c37c045eb68dfdbae0435a
pepenacho <herreragrej@proton.me>
2025-09-22 13:33:38 -0300

    add changes

--- src/main/java/com/bank/auth/auth_services/services/AuthService.java
+++ src/main/java/com/bank/auth/auth_services/services/AuthService.java
@@ -1,7 +1,30 @@
 package com.bank.auth.auth_services.services;
 
+import com.bank.auth.auth_services.dto.RegisterRequest;
+import com.bank.auth.auth_services.enums.AuthErrorCode;
+import com.bank.auth.auth_services.model.entity.AuthUser;
+import com.bank.auth.auth_services.repository.AuthUserRepositoryImpl;
+import com.bank.auth.auth_services.util.Result;
+import io.vavr.control.Try;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
+  private final AuthUserRepositoryImpl authUserRepository;
+  private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+  public Result<Void> register(RegisterRequest request) {
+   if (authUserRepository.existByUsername(request.username())) {
+     return Result.failure(AuthErrorCode.USERNAME_ALREADY_EXISTS.getMessage());
+    }
+
+    String hashedPassword = bCryptPasswordEncoder.encode(request.password());
+
+   Try<AuthUser> user = authUserRepository.save(request.username(), request.email(), hashedPassword);
+    return Result.success();
+
+  }
 }
