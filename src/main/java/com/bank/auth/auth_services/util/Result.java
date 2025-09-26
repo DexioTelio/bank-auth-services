4d8604dce13d8d1457a567393b3ac099ae299979
pepenacho <herreragrej@proton.me>
2025-09-26 10:46:45 -0300

    add changes

--- src/main/java/com/bank/auth/auth_services/util/Result.java
+++ src/main/java/com/bank/auth/auth_services/util/Result.java
@@ -1,53 +1,57 @@
 package com.bank.auth.auth_services.util;
 
+import com.bank.auth.auth_services.enums.AuthErrorCode;
 import com.bank.auth.auth_services.enums.interfaces.BaseErrorCode;
 
 import java.util.*;
 
 public record Result<T>(T value, boolean isSuccess, Set<BaseErrorCode> errorCode, List<String> errors) {
   public Result {
-    errors = Collections.unmodifiableList(errors);
-    validateState(isSuccess, errors);
+    errors = errors == null ? Collections.emptyList() : Collections.unmodifiableList(errors);
+    errorCode = errorCode == null ? Collections.emptySet() : Set.copyOf(errorCode);
   }
 
-  private void validateState(boolean isSuccess, List<String> errors) {
-    if (errors == null) errors = Collections.emptyList();
-
+  private static <T> Result<T> validate(Result<T> result) {
     List<String> inconsistencies = new ArrayList<>();
 
-    if (isSuccess && !errors.isEmpty()) {
+    if (result.isSuccess && !result.errors.isEmpty()) {
       inconsistencies.add("Successful result cannot have errors");
     }
-    if (!isSuccess && errors.isEmpty()) {
+    if (!result.isSuccess && result.errors.isEmpty()) {
       inconsistencies.add(("Failed result require at least 1 error"));
     }
     if (inconsistencies.isEmpty()) {
-      System.out.println("Result validation warning: " + inconsistencies);
+      return result;
+    } else {
+      return new Result<>(null,
+              false,
+              Set.of(AuthErrorCode.INTERNAL_ERROR),
+              List.of("Result validation warning: " + String.join(", ", inconsistencies)));
     }
   }
 
   public static <T> Result<T> success(T value) {
-    return new Result<>(value, true, Set.of(), null);
+    return validate(new Result<>(value, true, Set.of(), null));
   }
 
   public static Result<Void> success() {
-    return new Result<>(null, true, Set.of(), null);
+    return validate(new Result<>(null, true, Set.of(), null));
   }
 
   public static <T> Result<T> failure(List<String> errors) {
-    return new Result<>(null, false, Set.of(), errors);
+    return validate(new Result<>(null, false, Set.of(), errors));
   }
 
   public static <T> Result<T> failure(String errors) {
-    return new Result<>(null, false, Set.of(), Collections.singletonList(errors));
+    return validate(new Result<>(null, false, Set.of(), Collections.singletonList(errors)));
   }
 
   public static <T> Result<T> failure(Set<BaseErrorCode> errorCode, List<String> errors) {
-    return new Result<>(null, false, errorCode, errors);
+    return validate(new Result<>(null, false, errorCode, errors));
   }
 
   public static <T> Result<T> failure(BaseErrorCode errorCode, String errors) {
-    return new Result<>(null, false, Set.of(errorCode), Collections.singletonList(errors));
+    return validate(new Result<>(null, false, Set.of(errorCode), Collections.singletonList(errors)));
   }
 
   public List<String> getUniqueErrorList() {
