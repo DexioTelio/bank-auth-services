61c2978e60d2f4999e367f894276c1a741d09d72
pepenacho <herreragrej@proton.me>
2025-09-27 18:16:54 -0300

    add changes

--- src/main/java/com/bank/auth/auth_services/services/AuthService.java
+++ src/main/java/com/bank/auth/auth_services/services/AuthService.java
@@ -1,14 +1,14 @@
 package com.bank.auth.auth_services.services;
 
-import com.bank.auth.auth_services.dto.RegisterRequest;
 import com.bank.auth.auth_services.enums.AuthErrorCode;
 import com.bank.auth.auth_services.model.entity.AuthUser;
 import com.bank.auth.auth_services.repository.AuthUserRepositoryImpl;
 import com.bank.auth.auth_services.util.Result;
-import io.vavr.control.Try;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,21 +16,21 @@ public class AuthService {
   private final AuthUserRepositoryImpl authUserRepository;
   private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-  public Result<AuthUser> login(String username, String password) {
+  public Result<AuthUser, AuthErrorCode> login(String username, String password) {
     return authUserRepository.findByUserName(username)
             .map(optUser -> optUser
-                    .map(user -> {
+                    .map((user) -> {
                       // We verify the password
                       if (bCryptPasswordEncoder.matches(password, user.getPassword())) {
                         // We update the last login
                         authUserRepository.updateLastLogin(user.getId());
-                        return Result.success(user);
+                        return Result.<AuthUser, AuthErrorCode>success(user);
                       } else {
-                        return Result.<AuthUser>failure(AuthErrorCode.INVALID_CREDENTIALS.getMessage());
+                        return Result.<AuthUser, AuthErrorCode>failure(AuthErrorCode.INVALID_CREDENTIALS, AuthErrorCode.INVALID_CREDENTIALS.getMessage());
                       }
                     })
-                    .orElse(Result.<AuthUser>failure(AuthErrorCode.USER_NOT_FOUND.getMessage()))
+                    .orElse(Result.<AuthUser, AuthErrorCode>failure(AuthErrorCode.USER_NOT_FOUND, AuthErrorCode.USER_NOT_FOUND.getMessage()))
             )
-            .getOrElse(() -> Result.<AuthUser>failure("Unexpected error"));
+            .getOrElse(() -> Result.<AuthUser, AuthErrorCode>failure(AuthErrorCode.INTERNAL_ERROR, AuthErrorCode.INTERNAL_ERROR.getMessage()));
   }
 }
